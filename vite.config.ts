5e2ef4a0bbe9ef4bf2df0c8a8e34cc3f2a91201d
baiziyu-fe <baiziyu-fe@foxmail.com>
2022-08-20 20:47:40 +0800

    feat(unwatch): 关闭watch

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, './src') },
-    ]
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "./src/index.ts"),
-      name: '@ico-data/log-viewer',
-      formats: ['es'],
-      fileName: (name: string) => `log-viewer.${name}.js`,
-    },
-    watch: {
-      include: ["src/**/*"]
-    },
-    outDir: "lib/dist",
-    target: "es2015",
-    minify: "terser",
-    manifest: false,
-    sourcemap: true,
-  }
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: "@", replacement: path.resolve(__dirname, "./src") },
+		]
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "./src/index.ts"),
+			name: "@ico-data/log-viewer",
+			formats: ["es"],
+			fileName: (name: string) => `log-viewer.${name}.js`,
+		},
+		outDir: "lib/dist",
+		target: "es2015",
+		minify: "terser",
+		manifest: false,
+		sourcemap: true,
+	}
+});
