cdc5bf209c30ecae9e8fdb5a2f847dca24beebbc
baiziyu-fe <baiziyu-fe@foxmail.com>
2022-08-21 11:42:51 +0800

    feat(version): update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), dts()],
 	resolve: {
 		alias: [
 			{ find: "@", replacement: path.resolve(__dirname, "./src") },
